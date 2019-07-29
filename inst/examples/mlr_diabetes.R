771a0b92983e87b04a8548960c81207270595f3b
Magdalena Lang <magdalena.lang@viadee.de>
2019-07-29 17:59:40 +0200

    Update mlr_diabetes.R
    fix diabetes example

--- inst/examples/mlr_diabetes.R
+++ inst/examples/mlr_diabetes.R
@@ -1,18 +1,11 @@
-library("anchors")
+library(anchors)
 library(mlr)
 library(mlbench)
 
 data(PimaIndiansDiabetes)
 diabetes = PimaIndiansDiabetes
 
-# splitting the iris dataset into test and train data
-smp_size <- floor(0.75 * nrow (diabetes))
-set.seed(123)
-train_ind <- sample(seq_len(nrow(diabetes)), size = smp_size)
-train <- diabetes[train_ind, ]
-test <- diabetes[-train_ind, ]
-
-# our goal is to predict the species
+# our goal is to predict whether individuum has diabetes
 task = makeClassifTask(data = diabetes, target = "diabetes", id = "diabetes")
 
 # setting up a learner
@@ -24,19 +17,21 @@ model = mlr::train(learner = lrn.rpart, task = task)
 # Setting up a perturbation function. As we want explain a tabular instance (an observation in our dataset iris), we stick to a featureless tabular perturbation function
 perturbator = makePerturbFun("tabular.featureless")
 
-
 # discretizing the dataset
-
 discDiabetes = diabetes
 discDiabetes[,5] = NA
-discDiabetes = discretizeDF(discDiabetes)
-discDiabetes[,"insulin"] = discretize(diabetes[,"insulin"], breaks = 2 )
+discDiabetes = arules::discretizeDF(discDiabetes)
+discDiabetes[,"insulin"] = arules::discretize(diabetes[,"insulin"], breaks = 2 )
 
 
 # Explain model with anchors
-explainer = anchors(train, model, perturbator, discX = discDiabetes)
+explainer = anchors(diabetes, model, perturbator, discX = discDiabetes)
 
-explanations = explain(test[1,], explainer)
+explanations = explain(diabetes[1:2,], explainer)
 
 printExplanations(explainer, explanations)
+
+
+
+##TODO: plotExplanations(explanations)
 
