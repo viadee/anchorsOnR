9fe288e4cf44b6f54c561e9a72f0de1b5b4715d7
Magdalena Lang <magdalena.lang@viadee.de>
2019-07-29 11:32:22 +0200

    Update performance.R
    add checkmate::

--- R/performance.R
+++ R/performance.R
@@ -1,8 +1,8 @@
 performance_model = function(pred, measures, task = NULL, model = NULL, feats = NULL) {
   if (!is.null(pred)) {
-    assertClass(pred, classes = "Prediction")
+    checkmate::assertClass(pred, classes = "Prediction")
     if (!is.null(pred$task.desc)){
-      assertClass(pred$task.desc, classes = "TaskDesc")
+      checkmate::assertClass(pred$task.desc, classes = "TaskDesc")
       return(mlr::performance(pred, measures, task, model, feats))
     } else {
       #FIXME That's so bad
