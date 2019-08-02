f2987f7968dbc8a6101bb216dc86d8440fcbc2b2
thllwg <t_hell07@uni-muenster.de>
2019-08-02 17:02:17 +0200

    simplified example by changing scoping assignment and removing labels from explain method

--- inst/examples/mlr_iris.R
+++ inst/examples/mlr_iris.R
@@ -12,18 +12,19 @@ lrn = makeLearner("classif.rpart")
 # train the learner on the training set
 model = mlr::train(learner = lrn, task = task)
 
-# Visualize
+# visualize
 rpart.plot::rpart.plot(getLearnerModel(model))
 
-# Prepare bins: a list of bins per column, the nth-entry provides the bins of the nth-column
-# Per bin: define whether numeric (for now: only works with numeric), the cuts of the bins, and whether the right or the left border of a bin is included
-bins = list()
-r = sapply(1:(ncol(iris) - 1), function(x) {
-  bin <<- list()
+# Prepare bins:
+# a list of bins per column, the nth-entry provides the bins of the nth-column
+# Per bin: define whether numeric (for now: only works with numeric), the cuts
+# of the bins, and whether the right or the left border of a bin is included
+bins = lapply(1:(ncol(iris) - 1), function(x) {
+  bin <- list()
   cuts = arules::discretize(iris[, x], onlycuts = T)
-  bin$cuts <<- cuts[2:(length(cuts) - 1)]
-  bin$right <<- F
-  bins[[x]] <<- bin
+  bin$cuts <- cuts[2:(length(cuts) - 1)]
+  bin$right <- F
+  return(bin)
 })
 
 # Prepare explainer to explain model with anchors
@@ -31,6 +32,8 @@ explainer = anchors(iris, model, bins = bins, target = "Species")
 
 # Produce explanations for selected instances
 toExplain = iris[sample(1:nrow(iris), 20),]
-explanations = explain(toExplain, explainer, labels = predict(model, newdata = toExplain)$data$response)
+explanations = explain(toExplain, explainer)
 
+# print explanations
 printExplanations(explainer, explanations)
+
