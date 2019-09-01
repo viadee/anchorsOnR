c84c0bc0a0875a08917d107b87002c55f494beca
thllwg <t_hell07@uni-muenster.de>
2019-09-01 11:05:37 +0200

    test added, #43, #12

---
+++ tests/testthat/test-dataframe.R
@@ -0,0 +1,21 @@
+context("data.frame")
+library(MASS)
+library(mlr)
+
+test_that("anchors explanation only produces at max one entry per case and feature", {
+  # Split up the data set
+  iris_train <- iris
+
+  # Create Random Forest model on iris data
+  task = makeClassifTask(data = iris, target = "Species", id = "iris")
+  lrn = makeLearner("classif.rpart")
+  model = mlr::train(learner = lrn, task = task)
+
+
+  # Create explanation function
+  explainer <- anchors(iris_train, model)
+
+  # Explain new observation. This should yield a data.frame with at max 4 rows, because it's one case, that has 4 features, one label.
+  explanations <- explain(iris_train[1,], explainer)
+  expect_lt(nrow(explanations),5)
+})
